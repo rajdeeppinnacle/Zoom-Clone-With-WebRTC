6431c2b3837dcb89d2fec27fa5ea2df24b8a46fd
Rajdeep Singh <61683031+rajdeeppinnacle@users.noreply.github.com>
2021-05-17 12:26:20 +0530

    up

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 const socket = io('/')
-const videoGrid = document.getElementById('video-grid')
+const videoGrid = document.getElementById('screen')
 const myPeer = new Peer()
 const myVideo = document.createElement('video')
 myVideo.muted = true
